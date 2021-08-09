b3d4b61aec2c6ab86fe88ffca1932535a8cb121c
Puru Gupta <guptapuru304@gmail.com>
2021-08-09 13:26:18 +0530

    parse pokemon api response

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Lens
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Attoparsec
+import Network.HTTP.Req
+
+getResource :: MonadHttp m => m BsResponse
+getResource = req GET (https "app.pokemon-api.xyz" /: "pokemon" /: "random") NoReqBody bsResponse mempty
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runReq defaultHttpConfig $ do
+    r <- getResource
+    let 
+        resp = responseBody r
+
+    liftIO $ print $ parse json resp
+    liftIO $ print resp
